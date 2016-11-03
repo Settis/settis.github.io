3c7b093b5c997e47c37fc3a65e23e6b1a80933e3
Anton <Strannik.anton@gmail.com>
2016-11-03 16:13:50 +0300

    First

--- _plugins/pub-date.rb
+++
@@ -1,4 +0,0 @@
-Jekyll::Hooks.register :posts, :pre_render do |post|
-  //#post.data["pub_date"] = post.data["date"] if post.data["pub_date"] == {}
-  post.data["pub_date"] = post.data.fetch("pub_date", post.data["date"])
-end
