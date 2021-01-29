5bbf403a2ebb141ac94a57f163906980346a4286
Anton <strannik.anton@gmail.com>
2021-01-29 23:21:42 +0100

    How computer works

--- runLocally.sh
+++ runLocally.sh
@@ -2,4 +2,4 @@
 
 GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
 PATH="$PATH:$GEM_HOME/bin"
-jekyll s --draft --config _config.yml,_config_local.yml
+jekyll s --draft --config _config.yml,_config_local.yml -H 0.0.0.0 -l
